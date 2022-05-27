267091ca9cbcc59af8103b5f4077ddc7da3f77e9
Fabrício Cardoso <62123394+Fabricio-alt@users.noreply.github.com>
2022-05-27 10:48:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let numero1, numero2
+
+function somar(numero1, numero2){
+  return numero1 + numero2
+}
+
+function subtracao(numero1, numero2){
+  return numero1 - numero2
+}
+
+function multiplicacao(numero1, numero2){
+  return numero1 * numero2
+}
+
+function divisao(numero1, numero2){
+  return numero1 / numero2
+}
+
+
+console.log(somar(5,5))
+console.log(subtracao(10,5))
+console.log(multiplicacao(5,5))
+console.log(divisao(25,5))
